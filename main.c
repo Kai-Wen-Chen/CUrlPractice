74e9b625fa87e2abb0341043946443da043eccdf
陳楷文 <kelvin91734@gmail.com>
2022-10-02 13:26:36 +0800

    Code refinement

--- main.c
+++ main.c
@@ -2,42 +2,30 @@
 #include <stdlib.h>
 #include "url2file.c"
 
+#define FILE_NAME "data.out"
+
+
 int main(int argc, char *argv[]) {
-    static const char *pagefilename = "page.out";
-    FILE *pagefile;
+    const char *filename = FILE_NAME;
+    FILE *file;
 
     if(argc < 2) {
         printf("Usage: %s <URL>\n", argv[0]);
         return 1;
     }
 
-    //curl_global_init(CURL_GLOBAL_ALL);
-
     /* init the curl session */
     CURL *curl_handle = initCURLHandle();
-
-    /* set URL to get here */
     setURL(curl_handle, argv[1]);
     enableHTTP09(curl_handle, 1L);
-
-    /* Switch on full protocol/debug output while testing */
     enableVerbose(curl_handle, 1L);
 
-    /* disable progress meter, set to 0L to enable it */
-    curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
-
-    /* send all data to this function  */
+    /* send all data to file */
     setWriteFunction(curl_handle, NULL);
-
-    /* open the file */
-    pagefile = fopen(pagefilename, "wb");
-    if(pagefile) {
-
-        /* write the page body to this file handle */
-        writeData(curl_handle, pagefile);
-
-        /* close the header file */
-        fclose(pagefile);
+    file = fopen(filename, "wb");
+    if(file) {
+        writeData(curl_handle, file);
+        fclose(file);
     }
 
     /* cleanup curl stuff */
--- url2file.c
+++ url2file.c
@@ -34,56 +34,56 @@
 
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 {
-  size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
-  return written;
+    size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+    return written;
 }
 
 CURL* initCURLHandle() {
-  CURL* curl_handle;
-  curl_global_init(CURL_GLOBAL_ALL);
-  curl_handle = curl_easy_init();
-  return curl_handle;
+    CURL* curl_handle;
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl_handle = curl_easy_init();
+    return curl_handle;
 }
 
 void uninitCURLHandle(CURL* curl_handle) {
-  curl_easy_cleanup(curl_handle);
-  curl_global_cleanup();
+    curl_easy_cleanup(curl_handle);
+    curl_global_cleanup();
 }
 
 CURLcode setURL(CURL* curl_handle, const char* url) {
-  return curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+    return curl_easy_setopt(curl_handle, CURLOPT_URL, url);
 }
 
 CURLcode enableHTTP09(CURL* curl_handle, long allowed) {
-  return curl_easy_setopt(curl_handle, CURLOPT_HTTP09_ALLOWED, allowed);
+    return curl_easy_setopt(curl_handle, CURLOPT_HTTP09_ALLOWED, allowed);
 }
 
 CURLcode enableVerbose(CURL* curl_handle, long allowed) {
-  return curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, allowed);
+    return curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, allowed);
 }
 
 CURLcode setWriteFunction(CURL* curl_handle, size_t* writeFn) {
-  if (!writeFn)
-    return curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
-  else
-    return curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, *writeFn);
+    if (!writeFn)
+      return curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+    else
+      return curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, *writeFn);
 }
 
 CURLcode setWriteDataOutputFile(CURL* curl_handle, FILE* file) {
-  return curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, file);
+    return curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, file);
 }
 
 CURLcode writeData(CURL* curl_handle, FILE* file) {
-  if (!file) {
-    printf("Invalid file, please check if it exists\n");
-    return CURLE_WRITE_ERROR;
-  }
+    if (!file) {
+      printf("Invalid file, please check if it exists\n");
+      return CURLE_WRITE_ERROR;
+    }
 
-  CURLcode code = setWriteDataOutputFile(curl_handle, file);
-  if (code != CURLE_OK) {
-    printf("Write failed, error code: %d\n", code);
-    return code;
-  }
+    CURLcode code = setWriteDataOutputFile(curl_handle, file);
+    if (code != CURLE_OK) {
+      printf("Write failed, error code: %d\n", code);
+      return code;
+    }
 
-  return curl_easy_perform(curl_handle);
+    return curl_easy_perform(curl_handle);
 }
